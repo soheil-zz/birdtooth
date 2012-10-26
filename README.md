a438f11d0227f8c01cc00ae83bcfc602004781b0
Soheil <soheil@twitter.com>
2012-10-25 18:52:26 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+OSX Bluetooth Command Line Utility
+
+Provides the range(RSSI) for a nearby bluetooth device based on its bluetooth Mac address.
+
+Usage
+=====
+
+`birdtooth xxxxxxxxxxxx`
+
+the argument is the 12 character Mac address of the bluetooth device in hex format.
---
+++ birdtooth/birdtooth/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  birdtooth
+//
+//  Created by Soheil Yasrebi on 10/25/12.
+//  Copyright (c) 2012 Soheil Yasrebi. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
